a42e12c41271dc8a05ecbf23d921bbe72fec7107
a.degiacobbi <a.degiacobbi@A-DEGIACOBBI>
2025-02-17 17:55:50 +0100

    inizio wpf

---
+++ 37_WPFprojectUdemy/WpfAppUdemy/WpfAppUdemy/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppUdemy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyButton.FontSize = 50;
+            MyButton.Content = "Helloo";    //sovrascrive poichè è dopo l'InitializeComponent()
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // Ottieni l'URL dal parametro 'Uri' dell'evento
+            string url = e.Uri.AbsoluteUri;
+
+            // Usa Process.Start per aprire l'URL nel browser predefinito
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true // Apre l'URL nel browser
+            });
+
+            // Evita che l'evento venga ulteriormente gestito
+            e.Handled = true;
+
+        }
+    }
+}
